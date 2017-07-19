b944b835aab5c5b48f09c5067f447976787d7a30
meanhead <email@example.com>
2017-07-20 01:02:25 +0800

    ...

--- socket/app.js
+++ socket/app.js
@@ -4,6 +4,8 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
+
+
 app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
